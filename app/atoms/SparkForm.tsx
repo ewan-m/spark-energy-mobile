1542395640248e441abc53d053ca778ee40a1aec
ewan-m <ewanmorrrison@gmail.com>
2019-11-14 15:42:58 +0000

    Create a generic SparkForm component

---
+++ app/atoms/SparkForm.tsx
@@ -0,0 +1,17 @@
+import React, { FunctionComponent } from 'react';
+import { TextInput, TextInputProps } from 'react-native';
+
+export const SparkForm: FunctionComponent<TextInputProps> = (props) => (
+	<TextInput
+		style={Object.assign({
+			fontFamily: 'SparkOmnes-Regular',
+			fontSize: 20,
+			borderWidth: 1,
+			borderRadius: 10,
+			padding: 15,
+            borderColor: '#666',
+            color: '#666',
+            backgroundColor: '#fff'
+		}, props.style)}
+	></TextInput>
+);
