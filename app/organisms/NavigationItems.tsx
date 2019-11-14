3a9459363918cc32a9c6c25e4454a9e154f8613d
ewan-m <ewanmorrrison@gmail.com>
2019-11-14 15:47:33 +0000

    Add icons to navigation items

--- app/organisms/NavigationItems.tsx
+++ app/organisms/NavigationItems.tsx
@@ -1,25 +1,27 @@
 import { FooterTabProps } from '../molecules/FooterTab';
 import { colours } from '../styles/ColourPalette';
+import { IconHome, IconBill, IconQuestionMark, IconUser, IconSearch } from '../atoms/Icons';
+import React from 'react';
 
 export const navigationItems: Array<FooterTabProps> = [{
     colour: colours.blue,
     label: 'Home',
-    icon: 't'
+    icon: <IconHome fill={colours.blue} />
 }, {
     colour: colours.turquoise,
     label: 'Reading',
-    icon: 't'
+    icon: <IconSearch fill={colours.turquoise} />
 }, {
     colour: colours.hotPink,
     label: 'Bills',
-    icon: 't'
+    icon: <IconBill fill={colours.hotPink} />
 }, {
     colour: colours.magenta,
     label: 'Help',
-    icon: 't'
+    icon: <IconQuestionMark fill={colours.magenta} />
 }, {
     colour: colours.purple,
     label: 'Profile',
-    icon: 't',
+    icon: <IconUser fill={colours.purple} />,
     isActive: true
 }];
