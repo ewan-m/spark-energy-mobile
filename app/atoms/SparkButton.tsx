6f4c77ef75752dd0f4e9cf3e4a6ff770ec5096ce
ewan-m <ewanmorrrison@gmail.com>
2019-11-18 17:05:52 +0000

    Allow passing of child props to form and button component

--- app/atoms/SparkButton.tsx
+++ app/atoms/SparkButton.tsx
@@ -6,18 +6,23 @@ import { SparkTextSemiBold } from './SparkTextBold';
 
 export const SparkButton: FunctionComponent<SparkButtonProps> = (props) => (
 	<TouchableOpacity
-        {...props}
-		style={Object.assign({}, {
-			fontFamily: 'SparkOmnes-Regular',
-            fontSize: { big: 20, normal: 40 }[props.size],
-            padding: 15,
-            backgroundColor: colours.magenta,
-            fill: '#fff',
-            alignItems: 'center',
-            borderRadius: 10
-            
-		}, props.style)}
+		{...props}
+		style={Object.assign(
+			{},
+			{
+				fontFamily: 'SparkOmnes-Regular',
+				fontSize: { big: 20, normal: 40 }[props.size],
+				padding: 15,
+				backgroundColor: colours.magenta,
+				fill: '#fff',
+				alignItems: 'center',
+				borderRadius: 10,
+			},
+			props.style
+		)}
 	>
-		<SparkTextSemiBold size={props.size} style={{color: '#fff'}}>{props.children}</SparkTextSemiBold>
+		<SparkTextSemiBold size={props.size} style={{ color: '#fff' }}>
+			{props.children}
+		</SparkTextSemiBold>
 	</TouchableOpacity>
 );
--- app/atoms/SparkForm.tsx
+++ app/atoms/SparkForm.tsx
@@ -3,15 +3,20 @@ import { TextInput, TextInputProps } from 'react-native';
 
 export const SparkForm: FunctionComponent<TextInputProps> = (props) => (
 	<TextInput
-		style={Object.assign({
-			fontFamily: 'SparkOmnes-Regular',
-			fontSize: 20,
-			borderWidth: 1,
-			borderRadius: 10,
-			padding: 15,
-            borderColor: '#666',
-            color: '#666',
-            backgroundColor: '#fff'
-		}, props.style)}
+		{...props}
+		style={Object.assign(
+			{},
+			{
+				fontFamily: 'SparkOmnes-Regular',
+				fontSize: 20,
+				borderWidth: 1,
+				borderRadius: 10,
+				padding: 15,
+				borderColor: '#666',
+				color: '#666',
+				backgroundColor: '#fff',
+			},
+			props.style
+		)}
 	></TextInput>
 );
