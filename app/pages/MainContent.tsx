26352da36ae8fa003f7fff2a2e0564ef4f380ce6
Ewan Morrison <ewan.morrison@ovoenergy.com>
2019-11-29 15:56:01 +0000

    Add abstract background to main content of app

--- app/pages/MainContent.tsx
+++ app/pages/MainContent.tsx
@@ -1,24 +1,41 @@
 import React, { FunctionComponent, useState } from 'react';
-import { StatusBar, SafeAreaView, ScrollView } from 'react-native';
+import {
+	StatusBar,
+	SafeAreaView,
+	ScrollView,
+	ImageBackground,
+} from 'react-native';
 import { NavigationFooter } from '../organisms/NavigationFooter';
 import { navigationItems } from '../organisms/NavigationItems';
 import { NavigationNavigatorProps } from 'react-navigation';
 
-export const MainContent: FunctionComponent<NavigationNavigatorProps> = ({navigation}) => {
+export const MainContent: FunctionComponent<NavigationNavigatorProps> = ({
+	navigation,
+}) => {
 	const [currentNavIndex, setCurrentNavIndex] = useState(0);
 
 	return (
 		<>
 			<StatusBar barStyle="light-content" />
 			<SafeAreaView style={{ flex: 1 }}>
-				<ScrollView>
-					{navigationItems[currentNavIndex].page}
-				</ScrollView>
+				<ScrollView>{navigationItems[currentNavIndex].page}</ScrollView>
 				<NavigationFooter
 					selectedNavIndex={currentNavIndex}
 					setSelectedNavIndex={setCurrentNavIndex}
 					navigationItems={navigationItems}
 				></NavigationFooter>
+
+				<ImageBackground
+					source={require('../../assets/images/Energy_Abstract_Lime.jpg')}
+					style={{
+						position: 'absolute',
+						top: 0,
+						left: 0,
+						width: '100%',
+						height: '100%',
+						zIndex: -1,
+					}}
+				></ImageBackground>
 			</SafeAreaView>
 		</>
 	);
