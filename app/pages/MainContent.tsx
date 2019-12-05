da4963697edbb3b721fa092654cd87403ba5638a
ewan-m <ewanmorrrison@gmail.com>
2019-12-05 16:49:29 +0000

    Remove background from main page

--- app/pages/MainContent.tsx
+++ app/pages/MainContent.tsx
@@ -24,18 +24,6 @@ export const MainContent: FunctionComponent<NavigationNavigatorProps> = ({
 					setSelectedNavIndex={setCurrentNavIndex}
 					navigationItems={navigationItems}
 				></NavigationFooter>
-
-				<ImageBackground
-					source={require('../../assets/images/Energy_Abstract_Lime.jpg')}
-					style={{
-						position: 'absolute',
-						top: 0,
-						left: 0,
-						width: '100%',
-						height: '100%',
-						zIndex: -1,
-					}}
-				></ImageBackground>
 			</SafeAreaView>
 		</>
 	);
