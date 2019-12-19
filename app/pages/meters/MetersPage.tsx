d33356ed2b419caad100df6d265016009121ec01
ewan-m <ewanmorrrison@gmail.com>
2019-12-19 16:49:35 +0000

    Add meter read submission page

--- app/pages/meters/MetersPage.tsx
+++ app/pages/meters/MetersPage.tsx
@@ -2,13 +2,32 @@ import { FunctionComponent, useState, useEffect } from 'react';
 import React from 'react';
 import { SafeAreaView, View } from 'react-native';
 import { SparkText } from '../../atoms/SparkText';
-import { getMeters } from '../../api-communication/customer';
+import {
+	getCustomer,
+	MeterReadingFormData,
+} from '../../api-communication/customer';
+import { SparkForm } from '../../atoms/SparkForm';
+import { SparkCard } from '../../atoms/SparkCard';
+import { SparkButton } from '../../atoms/SparkButton';
+import { IconSun, IconMoon } from '../../atoms/Icons';
+import { HistoricalReading } from '../../api-communication/reading-history';
+import { SparkBulletList } from '../../molecules/SparkBulletList';
+import { SparkPageTitle } from '../../molecules/SparkPageTitle';
 
 export const MetersPage: FunctionComponent = () => {
 	const [isLoading, setIsLoading] = useState(true);
+	const [meters, setMeters] = useState([] as Array<MeterReadingFormData>);
+	const [historicalReads, setHistoricalReads] = useState(
+		[] as Array<HistoricalReading>
+	);
+
 	useEffect(() => {
-		getMeters().then((response) => {
-            setIsLoading(false);
+		getCustomer().then((response) => {
+			setIsLoading(false);
+
+			if (response.success && response.data) {
+				setMeters(response.data.meterReadingFormData);
+			}
 		});
 	}, []);
 	return (
@@ -22,16 +41,64 @@ export const MetersPage: FunctionComponent = () => {
 					width: '100%',
 				}}
 			>
+				<SparkPageTitle>Meters</SparkPageTitle>
+
+				{isLoading && <SparkText>Loading...</SparkText>}
+				{!isLoading && (
+					<SparkCard>
+						{meters.map((meter) => (
+							<View style={{ marginBottom: 20 }}>
+								<View
+									style={{
+										display: 'flex',
+										flexDirection: 'row',
+										justifyContent: 'space-between',
+										alignItems: 'flex-end',
+
+										marginBottom: 10,
+									}}
+								>
+									<View
+										style={{
+											display: 'flex',
+											flexDirection: 'row',
+											alignItems: 'center',
+										}}
+									>
+										{meter.e7 &&
+											(meter.e7 === 'Day' ? (
+												<IconSun width={30} height={30} />
+											) : (
+												<IconMoon width={30} height={30} />
+											))}
+										<SparkText size="big"> {meter.supply} </SparkText>
+									</View>
+									<SparkText size="small">{meter.label}</SparkText>
+								</View>
+								<SparkForm keyboardType="numeric" placeholder={meter.value} />
+							</View>
+						))}
+						<SparkButton size="normal">Submit readings</SparkButton>
+					</SparkCard>
+				)}
+
 				<SparkText
 					primary
 					semiBold
-					style={{ width: '100%', marginBottom: 20 }}
+					style={{ width: '100%', marginVertical: 20 }}
 					size="big"
 				>
-					Meters
+					Help
 				</SparkText>
-
-				{isLoading && <SparkText>Loading...</SparkText>}
+				<SparkCard>
+					<SparkBulletList
+						title="General Advice"
+						content={[
+							'Any numbers in red or after a decimal point can be ignored.',
+							'Meters are read from left to right, and zeros at the start should be included.',
+						]}
+					/>
+				</SparkCard>
 			</View>
 		</SafeAreaView>
 	);
