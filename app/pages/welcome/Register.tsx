f8ad27f33d62d9c74cae42e2ea56865b2c98f3fe
ewan-m <ewanmorrrison@gmail.com>
2020-01-08 13:57:31 +0000

    Add Registration tab to Welcome page

--- app/pages/welcome/Register.tsx
+++ app/pages/welcome/Register.tsx
@@ -1,4 +1,78 @@
-import React, { FunctionComponent } from 'react';
+import React, { FunctionComponent, useState } from 'react';
 import { SparkCard } from '../../atoms/SparkCard';
+import { SparkText } from '../../atoms/SparkText';
+import { SparkForm } from '../../atoms/SparkForm';
+import { SparkButton } from '../../atoms/SparkButton';
+import { colours } from '../../styles/ColourPalette';
 
-export const Register: FunctionComponent = () => <SparkCard></SparkCard>;
+export const Register: FunctionComponent = () => {
+	const [email, setEmail] = useState('');
+	const [password, setPassword] = useState('');
+	const [error, setError] = useState('');
+	return (
+		<>
+			<SparkText semiBold style={{ marginBottom: 2 }}>
+				Account Number
+			</SparkText>
+			<SparkText size="small" style={{ marginBottom: 5 }}>
+				This is a 10 digit number, usually begins with 9.
+			</SparkText>
+			<SparkForm
+				autoCompleteType="email"
+				keyboardType="email-address"
+				autoCapitalize="none"
+				style={{ marginBottom: 20 }}
+				onChangeText={setEmail}
+			></SparkForm>
+			<SparkText semiBold style={{ marginBottom: 5 }}>
+				Postcode
+			</SparkText>
+			<SparkForm
+				autoCompleteType="email"
+				keyboardType="email-address"
+				autoCapitalize="none"
+				style={{ marginBottom: 20 }}
+				onChangeText={setEmail}
+			></SparkForm>
+			<SparkText semiBold style={{ marginBottom: 2 }}>
+				Email
+			</SparkText>
+			<SparkText size="small" style={{ marginBottom: 5 }}>
+				Choose your email for your new online account.
+			</SparkText>
+			<SparkForm
+				autoCompleteType="email"
+				keyboardType="email-address"
+				autoCapitalize="none"
+				style={{ marginBottom: 20 }}
+				onChangeText={setEmail}
+			></SparkForm>
+
+			<SparkText semiBold style={{ marginBottom: 2 }}>
+				Password
+			</SparkText>
+			<SparkText size="small" style={{ marginBottom: 5 }}>
+				Include a capital letter, number and special character. It has to be at
+				least 8 characters long.
+			</SparkText>
+			<SparkForm
+				autoCapitalize="none"
+				secureTextEntry={true}
+				autoCompleteType="password"
+				onChangeText={setPassword}
+			></SparkForm>
+
+			{error !== '' && (
+				<SparkText
+					style={{ color: colours.failureRed, marginTop: 5, marginBottom: 20 }}
+				>
+					{error}
+				</SparkText>
+			)}
+
+			<SparkButton style={{ marginTop: 20 }} onPress={() => {}} size="normal">
+				Register
+			</SparkButton>
+		</>
+	);
+};
--- app/pages/welcome/Welcome.tsx
+++ app/pages/welcome/Welcome.tsx
@@ -1,19 +1,94 @@
-import React, { FunctionComponent } from 'react';
-import { SafeAreaView, View } from 'react-native';
+import React, { FunctionComponent, useState } from 'react';
+import {
+	SafeAreaView,
+	View,
+	Dimensions,
+	Image,
+	ScrollView,
+} from 'react-native';
+import { TabView, SceneMap, TabBar } from 'react-native-tab-view';
 import { Login } from './Login';
+import { Register } from './Register';
+import { SparkText } from '../../atoms/SparkText';
+import { SparkCard } from '../../atoms/SparkCard';
+import { colours } from '../../styles/ColourPalette';
 import { SparkPageContainer } from '../../molecules/SparkPageContainer';
-import { SparkPageTitle } from '../../molecules/SparkPageTitle';
 
-export const Welcome: FunctionComponent<any> = (props) => (
-	<SafeAreaView>
-		<SparkPageContainer>
-			<View style={{width: '100%'}}>
-				<SparkPageTitle>Login</SparkPageTitle>
-				<Login onLoginSuccess={() => props.navigation.navigate('MainContent')} />
-			</View>
-			<View>
-				<SparkPageTitle>Register</SparkPageTitle>
-			</View>
-		</SparkPageContainer>
-	</SafeAreaView>
+const LoginPage = () => (
+	<View style={{ padding: 15 }}>
+		<Login />
+	</View>
 );
+
+const RegisterPage = () => (
+	<View style={{ padding: 15 }}>
+		<Register />
+	</View>
+);
+
+const renderTabBar = (props: any) => (
+	<TabBar
+		{...props}
+		indicatorStyle={{
+			backgroundColor: colours.magenta,
+			height: 2,
+		}}
+		style={{ backgroundColor: 'transparent', marginBottom: 20, elevation: 0 }}
+		renderLabel={({ route, focused }) => (
+			<SparkText
+				size="big"
+				style={{
+					color: focused ? colours.magenta : colours.secondaryText,
+				}}
+			>
+				{route.title}
+			</SparkText>
+		)}
+	/>
+);
+
+export const Welcome: FunctionComponent<any> = () => {
+	const [index, setIndex] = useState(0);
+	const [routes] = useState([
+		{ key: 'first', title: 'Login' },
+		{ key: 'second', title: 'Register' },
+	]);
+	const renderScene = SceneMap({
+		first: LoginPage,
+		second: RegisterPage,
+	});
+	const initialLayout = { width: Dimensions.get('window').width };
+
+	return (
+		<SafeAreaView>
+			<ScrollView>
+				<SparkPageContainer>
+					<View style={{ width: '100%', alignItems: 'center', paddingVertical: 30 }}>
+						<Image
+							style={{ width: 250, height: 150, marginBottom: 5 }}
+							source={require('../../../assets/images/logo-transparent.png')}
+						></Image>
+					</View>
+					<SparkCard style={{ padding: 0, marginBottom: 20 }}>
+						<TabView
+							renderTabBar={renderTabBar}
+							navigationState={{ index, routes }}
+							renderScene={renderScene}
+							onIndexChange={setIndex}
+							initialLayout={initialLayout}
+							style={{
+								marginBottom: 20,
+								width: '100%',
+							}}
+						/>
+					</SparkCard>
+					<View style={{ paddingHorizontal: 15, paddingVertical: 5, width: '100%' }}>
+						<SparkText size="small">
+							© Spark Energy {new Date().getFullYear()}
+						</SparkText>
+					</View>
+				</SparkPageContainer>
+			</ScrollView>
+		</SafeAreaView>
+	);
+};
