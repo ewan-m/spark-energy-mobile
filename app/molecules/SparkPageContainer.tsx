cbb0d014489d5ffeab9537d5da09f215f5f036cf
ewan-m <ewanmorrrison@gmail.com>
2020-01-08 14:02:28 +0000

    Add a subtle grow animation to all page containers

--- app/molecules/SparkPageContainer.tsx
+++ app/molecules/SparkPageContainer.tsx
@@ -1,16 +1,34 @@
-import React, { FunctionComponent } from 'react';
-import { View } from 'react-native';
+import React, { FunctionComponent, useEffect, useState } from 'react';
+import { Animated, Easing } from 'react-native';
+import { colours } from '../styles/ColourPalette';
 
-export const SparkPageContainer: FunctionComponent = ({ children }) => (
-	<View
-		style={{
-			padding: 15,
-			alignItems: 'center',
-			justifyContent: 'space-evenly',
-			height: '100%',
-			width: '100%',
-		}}
-	>
-		{children}
-	</View>
-);
+export const SparkPageContainer: FunctionComponent = ({ children }) => {
+	const [grow] = useState(new Animated.Value(0.98));
+	useEffect(() => {
+		Animated.timing(grow, {
+			toValue: 1,
+			duration: 250,
+			useNativeDriver: true,
+			easing: Easing.cubic,
+		}).start();
+	}, []);
+	return (
+		<Animated.View
+			style={{
+				padding: 15,
+				alignItems: 'center',
+				backgroundColor: colours.greyLight,
+				justifyContent: 'space-evenly',
+				height: '100%',
+				width: '100%',
+				transform: [
+					{
+						scale: grow,
+					},
+				],
+			}}
+		>
+			{children}
+		</Animated.View>
+	);
+};
