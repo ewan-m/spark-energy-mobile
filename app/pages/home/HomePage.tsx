3711bee98b5c57e0e324e36372bad7f16a39cac3
ewan-m <ewanmorrrison@gmail.com>
2020-01-08 14:07:15 +0000

    Remove the MaskedText from the home page

--- app/pages/home/HomePage.tsx
+++ app/pages/home/HomePage.tsx
@@ -1,34 +1,37 @@
 import React, { FunctionComponent } from 'react';
 import { SafeAreaView } from 'react-navigation';
 import { SparkText } from '../../atoms/SparkText';
-import { SparkMaskedText } from '../../molecules/SparkMaskedText';
 import { SparkCard } from '../../atoms/SparkCard';
 import { SparkPageContainer } from '../../molecules/SparkPageContainer';
 import { SparkImagePanel } from '../../molecules/SparkImagePanel';
-
+import { SparkPageTitle } from '../../molecules/SparkPageTitle';
 
 export const HomePage: FunctionComponent = () => {
 	return (
 		<SafeAreaView>
 			<SparkPageContainer>
 				<SparkCard style={{ marginBottom: 20 }}>
-					<SparkMaskedText textAlign="flex-start">Meters</SparkMaskedText>
+					<SparkPageTitle>Meters</SparkPageTitle>
 					<SparkText>
 						Submit meter readings to help improve the accuracy of your bills. Look at
 						your previous reads to get an idea of how much energy you use.
 					</SparkText>
 				</SparkCard>
-				<SparkImagePanel panelImage={require("../../../assets/images/Couple_breakfast_iPad.png")} />
+				<SparkImagePanel
+					panelImage={require('../../../assets/images/Couple_breakfast_iPad.png')}
+				/>
 				<SparkCard style={{ marginBottom: 20 }}>
-					<SparkMaskedText textAlign="flex-end">Payments</SparkMaskedText>
+					<SparkPageTitle>Payments</SparkPageTitle>
 					<SparkText>
 						Take a look at your payment history and check your balance.
 					</SparkText>
 				</SparkCard>
-				
-				<SparkImagePanel panelImage={require("../../../assets/images/mother-child.png")} />
+
+				<SparkImagePanel
+					panelImage={require('../../../assets/images/mother-child.png')}
+				/>
 				<SparkCard style={{ marginBottom: 20 }}>
-					<SparkMaskedText textAlign="flex-start">Profile</SparkMaskedText>
+					<SparkPageTitle>Profile</SparkPageTitle>
 					<SparkText>
 						Check out the personal information we have for your account and make sure
 						everything is up to date.
